5dd2e15808f8aaa35554886aa150ec895d4e0cc2
Arushi Katyal <katy.arushi@gmail.com>
2022-04-12 02:39:21 -0400

    add api and useEffect and state

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+
 function Popular() {
+	const [popular, setPopular] = useState([]);
+
+	useEffect(() => {
+		getPopular();
+	}, []); // run getPopular once, on mount
+
+	const getPopular = async () => {
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+		);
+		const data = await api.json();
+		setPopular(data.recipes);
+	};
+
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map(recipe => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
-export default Popular
+export default Popular;
