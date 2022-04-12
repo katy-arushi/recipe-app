81a4af431d283ab8e65c0f6bf946ec03c52427d1
Arushi Katyal <katy.arushi@gmail.com>
2022-04-12 14:26:08 -0400

    add nav link

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -2,6 +2,8 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import '@splidejs/splide/dist/css/splide.min.css';
+import { Link } from 'react-router-dom';
+
 
 function Veggie() {
 
@@ -43,9 +45,11 @@ function Veggie() {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={'/recipe/' + recipe.id}></Link>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  <Link/>
                 </Card>
               </SplideSlide>
             );
