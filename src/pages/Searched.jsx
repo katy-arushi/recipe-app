7422566cf6e14c53bcd8ec6f90de81bb86320229
Arushi Katyal <katy.arushi@gmail.com>
2022-04-13 12:21:11 -0400

    edit var name

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
-
 
 function Searched() {
-  
+
   const [searchedRecipes, setSearchedRecipes] = useState([]);
   let params = useParams();
 
-  const getSearched = async (query) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${query}`);
+  const getSearched = async (name) => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
     const recipes = await data.json();
     setSearchedRecipes(recipes.results);
   };
