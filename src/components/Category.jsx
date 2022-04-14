6aeb6c39e965f42fdeee6878c1e9fa5ecae7159b
Arushi Katyal <katy.arushi@gmail.com>
2022-04-13 23:23:24 -0400

    add new cuisines and icons

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,26 +1,38 @@
-import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
-import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import { FaPizzaSlice, FaHamburger, FaCheese } from 'react-icons/fa';
+import { GiIndianPalace, GiChopsticks, GiGreekTemple, GiCookingPot } from 'react-icons/gi';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 function Category() {
   return (
 		<List>
-			<SLink to={'/cuisine/italian'}>
-				<FaPizzaSlice />
-				<h4>Italian</h4>
-			</SLink>
 			<SLink to={'/cuisine/american'}>
 				<FaHamburger />
 				<h4>American</h4>
 			</SLink>
+			<SLink to={'/cuisine/french'}>
+        <FaCheese />
+				<h4>French</h4>
+			</SLink>
+			<SLink to={'/cuisine/greek'}>
+        <GiGreekTemple />
+				<h4>Greek</h4>
+			</SLink>
 			<SLink to={'/cuisine/indian'}>
-				<GiNoodles />
+        <GiIndianPalace />
 				<h4>Indian</h4>
+			</SLink>
+			<SLink to={'/cuisine/italian'}>
+				<FaPizzaSlice />
+				<h4>Italian</h4>
 			</SLink>
 			<SLink to={'/cuisine/japanese'}>
 				<GiChopsticks />
 				<h4>Japanese</h4>
+			</SLink>
+			<SLink to={'/cuisine/spanish'}>
+        <GiCookingPot />
+				<h4>Spanish</h4>
 			</SLink>
 		</List>
 	);
@@ -40,14 +52,15 @@ const SLink = styled(NavLink)`
   margin-right: 2rem;
   text-decoration: none;
   background: linear-gradient(35deg, #494949, #313131);
-  width: 6rem;
-  height: 6rem;
+  width: 8rem;
+  height: 8rem;
   cursor: pointer;
   transform: scale(0.8);
 
   h4 {
     color: white;
-    font-size: 0.8rem;
+    font-size: 1rem;
+    padding-top: 0.5rem;
   }
 
   svg {
