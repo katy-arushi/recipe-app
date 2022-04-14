2d280697649b1e70ac9ccf8963ac1b2584a46ea9
Arushi Katyal <katy.arushi@gmail.com>
2022-04-13 23:02:34 -0400

    make api request only veg recipes

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -9,7 +9,7 @@ function Cuisine() {
   let params = useParams();
 
   const getCuisine = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`);
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&diet=vegetarian&cuisine=${name}`);
     const recipes = await data.json();
     setCuisine(recipes.results);
   }
